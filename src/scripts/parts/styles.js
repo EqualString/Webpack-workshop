088c8ccbe37048dbce60b580749689595daf41ea
EqualString <alen.egredzija@factory.hr>
2020-06-09 20:11:43 +0200

    FEATURE: Added code for SASS Workshop

--- src/scripts/parts/styles.js
+++ src/scripts/parts/styles.js
@@ -2,9 +2,5 @@
  * Styles entry point
  */
 
-// Classic import
-import '../../styles/style.css';
-import '../../styles/style.scss';
-
 // Import using aliases/resolve
-import 'styles/style.sass';
+import 'styles/sass/main.sass';
